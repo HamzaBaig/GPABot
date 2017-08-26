6aa8a82c99d22650bf5beea5d30f23ed3d07ea88
Inzamam Malik <malikasinger1@gmail.com>
2017-08-26 17:41:28 +0500

    send message on enter key

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
- import { Http, Headers } from "@angular/http";
-import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import { Http, Headers } from "@angular/http";
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
@@ -10,58 +10,52 @@ import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database
 export class HomePage {
   msg;
   imgurl;
-   itemList = document.getElementById("item-list");
-    //char;
+  itemList = document.getElementById("item-list");
+  //char;
   arr = [];
   convo: FirebaseListObservable<any>;
-  constructor(public navCtrl: NavController,private http: Http, private db: AngularFireDatabase) {
-    this.convo = this.db.list('/conversation',{preserveSnapshot: true});
-    this.convo.subscribe(snapshots =>{
+  constructor(public navCtrl: NavController, private http: Http, private db: AngularFireDatabase) {
+    this.convo = this.db.list('/conversation', { preserveSnapshot: true });
+    this.convo.subscribe(snapshots => {
       this.arr = [];
       snapshots.forEach(element => {
-       
+
         this.arr.push(element.val());
-        console.log("objjjj",this.arr);
+        console.log("objjjj", this.arr);
       });
     })
   };
-  ngOnChanges(){
+  ngOnChanges() {
     console.log('cheannndheh');
-    
+
     this.itemList.scrollTop = this.itemList.scrollHeight
-    
+
   }
-  sendMessage(){
+  sendMessage() {
     console.log(this.msg);
-    let send = {name: 'you' , imgurl: '../assets/images/user.png', msg: this.msg}
+    let send = { name: 'you', imgurl: '../assets/images/user.png', msg: this.msg }
     this.db.list('/conversation').push(send);
     this.sendBot();
     this.msg = '';
   }
-  sendBot(){
-    let bot = {name: 'bot' , imgurl: '../assets/images/bot.png' , msg: ''}
+  sendBot() {
+    let bot = { name: 'bot', imgurl: '../assets/images/bot.png', msg: '' }
     let headers = new Headers()
     headers.append(
-      'Authorization' , 'Bearer 0fc57fb62042401a9d56268a993ce643'
+      'Authorization', 'Bearer 0fc57fb62042401a9d56268a993ce643'
     )
-    this.http.get('https://api.api.ai/v1/query?v=20150910&query=' + this.msg + '&lang=en&sessionId=a5292bd2-3af8-4ea2-b8b2-614db278e333&timezone=Asia/Karachi',{headers: headers})
-    .subscribe((response)=>{
-      bot.msg = response.json().result.fulfillment.speech;
-      this.db.list('/conversation').push(bot);
-    })
+    this.http.get('https://api.api.ai/v1/query?v=20150910&query=' + this.msg + '&lang=en&sessionId=a5292bd2-3af8-4ea2-b8b2-614db278e333&timezone=Asia/Karachi', { headers: headers })
+      .subscribe((response) => {
+        bot.msg = response.json().result.fulfillment.speech;
+        this.db.list('/conversation').push(bot);
+      })
   }
-  // check(event){
-  //   //console.log("evenntttt",event);
-  //   var char = event[event.length-1];
-  //   //event = event.substring(0,-1);
-   
-  //   if(char.charCodeAt(0) == 13){
-  //     console.log("char",char);
-  //     this.sendMessage();
-  //   }
-    
-  //}
-  // setTimeout(()=>{
+  check(event) {
+    console.log("event.code", event.code);
+    if (event.code === "Enter") {
+      event.preventDefault();
+      this.sendMessage();
+    }
+  }
 
-  // },3000);
 }
