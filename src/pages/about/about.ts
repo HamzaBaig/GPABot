fee7a1361bb0855584e41fd33efff474b86f1b95
hammadyounas <m.hammadyounas@outlook.com>
2017-08-26 14:03:43 +0500

    card complete

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
-  constructor(public navCtrl: NavController) {
-
+  orders: FirebaseListObservable<any>
+  arr = [];
+  constructor(public navCtrl: NavController, private db: AngularFireDatabase) {
+    this.orders = this.db.list('/orders',{preserveSnapshot: true});
+    this.orders.subscribe(snapshots =>{
+      snapshots.forEach(element => {
+        this.arr.push(element.val());
+      });
+    })
   }
 
 }
